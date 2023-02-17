e8af4512f60fae98c635cc6b3e0e6210e4bc5b3f
Ashish03-k <ashishkhandagale999@gmail.com>
2023-02-17 17:56:23 +0530

    commited assignments
    completed javascript  assignments

---
+++ 01_variablesBasicAssi.js
@@ -0,0 +1,20 @@
+
+var PIN =  413306;
+var myName = "Ashish";
+var isMarried = false;
+
+console.log("PIN :" , PIN , "type:", typeof PIN);
+console.log("Name :" , myName , "type:", typeof myName);
+console.log("married status :" , isMarried , "type:", typeof isMarried);
+
+
+console.log("**************************************************************");
+
+console.log("******Differance Between null & undefined*******");
+
+
+console.log("Null :-");
+console.log("Whenever we declaire a variable and initilaize it with null value these variable is known as NULL.");
+
+console.log("undefined :-");
+console.log("Whenever we just declare a variable and not initilize it and try to print this variable then it will print undefined as a output.");
---
+++ variableAssignment.js
@@ -0,0 +1,35 @@
+
+console.log("--------------Before Initilizatin--------------");
+var myFullName;
+console.log(myFullName);
+
+console.log("--------------After Initilizatin--------------");
+var myFullName = "Ashish Khandagale";
+console.log(myFullName);
+
+console.log("-------------- Updated Full Name --------------"); 
+
+var myFullName = "Ashish Baban Khandagale";
+console.log(myFullName);
+
+console.log("-------------- Declaration And Initialization variable --------------");  
+
+var myLove = "I Love Only JS";
+console.log(myLove);
+
+
+console.log("-------------- Personal Details  --------------");  
+
+var firstName = "Ashish";
+var lastName = "Khandagale";
+var age = "23";
+var address = "Sangola";
+var pincode = "413306";
+var gender = "male";
+var isMarried = "UunMarried";
+
+console.log("-----------------------------------------------------------------------");
+console.log("firstName | lastName | age | address | pincode | gender | isMarried");
+console.log(firstName ,"   |", lastName ," |", age ," |", address ,"|", pincode ," |", gender ," |", isMarried);
+console.log("-----------------------------------------------------------------------"); 
+
